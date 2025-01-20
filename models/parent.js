2b6903902ff2a57066e34528b8a5472f0127aca1
GhanimAlhashash <Ghanim-alhashash@hotmail.com>
2025-01-20 09:34:20 +0300

    adding balance

--- models/parent.js
+++ models/parent.js
@@ -3,6 +3,7 @@ const { model, Schema } = require('mongoose');
 const ParentSchema = new Schema({
     Pname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    balance: { type: Number, default: 1400 },
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     role: { type: String, default: 'parent' } // Role field with default value 'parent'
 });
